a068a9b9a52f5bf7f744db02690be1a0bf4d8c78
waynechen <szchen@tfs.ca>
2023-05-30 22:31:06 -0400

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Wayne Chen on 2023-05-30.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
